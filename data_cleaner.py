c75f58de9fd78739b15eb1fa1adc31ece867aa0e
Eddison Pham <98775888+eddisonpham@users.noreply.github.com>
2024-05-19 15:47:39 -0400

    Update data_cleaner.py

--- data_cleaner.py
+++ data_cleaner.py
@@ -2,7 +2,7 @@ import csv
 with open('sample_output.csv','r') as csv_file:
   csv_reader = csv.reader(csv_file)
 
-  with open("cleaned_output.csv",'w') as new_file:
+  with open("cleaned_output.csv",'w',newline='') as new_file:
     csv_writer = csv.writer(new_file, delimiter=',')
     for line in csv_reader:
       if (len(line) == 13):
