1fafada2531fa41be2cae89515fe5d072b11ff6e
oskip0906 <oscar.pang050906@gmail.com>
2024-05-19 17:24:30 -0400

    Finished data preprocessing section

--- data_cleaner.py
+++ data_cleaner.py
@@ -5,7 +5,7 @@ with open('sample_output.csv','r') as csv_file:
 
   with open("cleaned_output.csv",'w',newline='') as new_file:
     csv_writer = csv.writer(new_file, delimiter=',')
-    line = ['T1C1','T1C2','T1C3','T1C4','T1C5','T2C1','T2C2','T2C3','T2C4','T2C5','GD','WL']
+    line = ['T1C1','T1C2','T1C3','T1C4','T1C5','T2C1','T2C2','T2C3','T2C4','T2C5','GD','Target']
     csv_writer.writerow(line)
     for line in csv_reader:
       if (len(line) == 13):
--- trainer.py
+++ trainer.py
@@ -1,4 +1,5 @@
-# importing libraries
+''' importing libraries '''
+
 import sys
 sys.path.append('libraries')
 
@@ -12,9 +13,38 @@ from sklearn.pipeline import Pipeline
 from sklearn.model_selection import RandomizedSearchCV, train_test_split
 from sklearn.metrics import accuracy_score, confusion_matrix, precision_score, recall_score, ConfusionMatrixDisplay
 
-# Preprocessing
-df = pd.read_csv('cleaned_output.csv')
-print(df.head())
+'''Preprocessing'''
 
-feature_columns = df.columns[:-2]
-target_column = df.columns[-1]
+df = pd.read_csv('cleaned_output.csv')
+# print(df.head())
+
+categorical_columns = df.columns[:-2]
+numerical_column = df.columns[-2]
+
+team_1 = categorical_columns[:5]
+team_2 = categorical_columns[5:]
+
+# initlize encoder and scaler
+encoder = OneHotEncoder(sparse_output=True)
+scaler = StandardScaler()
+
+# encode the teams seperately
+encoded_team_1 = encoder.fit_transform(df[team_1])
+encoded_team_1_df = pd.DataFrame.sparse.from_spmatrix(encoded_team_1, columns=encoder.get_feature_names_out(team_1))
+encoded_team_2 = encoder.fit_transform(df[team_2])
+encoded_team_2_df = pd.DataFrame.sparse.from_spmatrix(encoded_team_2, columns=encoder.get_feature_names_out(team_2))
+
+# scale the numerical data 
+scaled_GD = scaler.fit_transform(df[[numerical_column]])
+scaled_GD_df = pd.DataFrame(scaled_GD, columns=[numerical_column])
+
+# replace the original categorical columns with the encoded columns in the dataframe
+df = df.drop(categorical_columns, axis=1)
+df = df.drop(numerical_column, axis=1)
+df = pd.concat([encoded_team_1_df, encoded_team_2_df, scaled_GD_df, df], axis=1)
+# print(df.head())
+
+features = df.drop('Target', axis=1)
+target = df['Target']
+
+''' Training the model '''
