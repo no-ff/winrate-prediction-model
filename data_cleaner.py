2528416403d9782b7b8604219612a78fc4085c74
oskip0906 <oscar.pang050906@gmail.com>
2024-05-20 18:41:00 -0400

    fine tuned the version 1 model

--- data_cleaner.py
+++ data_cleaner.py
@@ -1,12 +1,16 @@
 import csv
 
-with open('csv_files/sample_output.csv','r') as csv_file:
-  csv_reader = csv.reader(csv_file)
+def clean_data():
+  
+  with open('csv_files/sample_output.csv','r') as csv_file:
+    csv_reader = csv.reader(csv_file)
 
-  with open("csv_files/cleaned_output.csv",'w',newline='') as new_file:
-    csv_writer = csv.writer(new_file, delimiter=',')
-    line = ['T1C1','T1C2','T1C3','T1C4','T1C5','T2C1','T2C2','T2C3','T2C4','T2C5','GD']
-    csv_writer.writerow(line)
-    for line in csv_reader:
-      if (len(line) == 13):
-        csv_writer.writerow(line[:-2])
+    with open("csv_files/cleaned_output.csv",'w',newline='') as new_file:
+      csv_writer = csv.writer(new_file, delimiter=',')
+      line = ['T1C1','T1C2','T1C3','T1C4','T1C5','T2C1','T2C2','T2C3','T2C4','T2C5','GD']
+      csv_writer.writerow(line)
+      for line in csv_reader:
+        if (len(line) == 13):
+          csv_writer.writerow(line[:-2])
+
+clean_data()
--- trainer.py
+++ trainer.py
@@ -9,61 +9,98 @@ import joblib
 
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.preprocessing import OneHotEncoder, MinMaxScaler
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import r2_score
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.metrics import make_scorer, r2_score
 
-''' preprocessing ''' 
+from data_cleaner import clean_data
 
-df = pd.read_csv('csv_files/cleaned_output.csv')
-# print(df.head())
+''' hyperparameters tuning '''
 
-feature_names = df.columns[:-1]
-feature_values = df[feature_names].values
-target = df['GD'].values
+# parameters = {
+#     'n_estimators': [100, 300, 500],
+#     'max_features': [None, 'sqrt', 'log2'],
+#     'max_depth': [10, 20, 30, None],
+#     'min_samples_split': [2, 5, 10, 20],
+#     'min_samples_leaf': [1, 2, 4, 8],
+#     'criterion': ['poisson', 'absolute_error', 'friedman_mse', 'squared_error'],
+# }
 
-# initlize encoder and scaler
-encoder = OneHotEncoder(sparse_output=True)
-scaler = MinMaxScaler(feature_range=(-1, 1))
+''' hyperparameters (current best) '''
+parameters = {
+    'n_estimators': 100,
+    'max_features': 'sqrt',
+    'max_depth': 20,
+    'min_samples_split': 20,
+    'min_samples_leaf': 1,
+    'criterion': 'friedman_mse',
+}
 
-# encode the teams seperately
-encoding = encoder.fit_transform(feature_values)
-features = pd.DataFrame.sparse.from_spmatrix(encoding, columns=encoder.get_feature_names_out(feature_names))
+''' main model trainer '''
 
-# print(features)
-# print(target)
+def train():
 
-X_train,X_test,y_train,y_test = train_test_split(features, target, test_size=0.2, random_state=0)
+    ''' preprocessing ''' 
 
-# print(X_train)
-# print(X_test)
-# print(y_train)
-# print(y_test)
+    # clean the data
+    # clean_data()
 
-scaler = MinMaxScaler(feature_range=(-1, 1))
-y_train = y_train.reshape(-1, 1)
-y_test = y_test.reshape(-1, 1)
-scaler.fit(y_train)
-y_train = scaler.transform(y_train).ravel()
-y_test = scaler.transform(y_test).ravel()
+    df = pd.read_csv('csv_files/cleaned_output.csv')
+    # print(df.head())
 
-# print(y_train)
-# print(y_test)
+    feature_names = df.columns[:-1]
+    feature_values = df[feature_names].values
+    target = df['GD'].values
 
-''' training the model '''
+    # initlize encoder and scaler
+    encoder = OneHotEncoder(sparse_output=True)
+    scaler = MinMaxScaler(feature_range=(-1, 1))
 
-regressor = RandomForestRegressor(n_estimators=1000, random_state=0)
-regressor.fit(X_train, y_train)
+    # encode the teams seperately
+    encoding = encoder.fit_transform(feature_values)
+    features = pd.DataFrame.sparse.from_spmatrix(encoding, columns=encoder.get_feature_names_out(feature_names))
 
-''' evaluating the model '''
+    # print(features)
+    # print(target)
 
-y_pred = regressor.predict(X_test)
-np.set_printoptions(precision=2)
-print(np.concatenate((y_pred.reshape(len(y_pred),1),y_test.reshape(len(y_test),1)),1))
+    X_train,X_test,y_train,y_test = train_test_split(features, target, test_size=0.2, random_state=0)
 
-print(r2_score(y_test, y_pred))
+    # print(X_train)
+    # print(X_test)
+    # print(y_train)
+    # print(y_test)
 
-''' saving the model '''
+    scaler = MinMaxScaler(feature_range=(-1, 1))
+    y_train = y_train.reshape(-1, 1)
+    y_test = y_test.reshape(-1, 1)
+    scaler.fit(y_train)
+    y_train = scaler.transform(y_train).ravel()
+    y_test = scaler.transform(y_test).ravel()
 
-joblib.dump(regressor, 'pickle_files/model.pkl')
-joblib.dump(scaler, 'pickle_files/scaler.pkl')
-joblib.dump(encoder, 'pickle_files/encoder.pkl')
+    # print(y_train)
+    # print(y_test)
+
+    ''' training '''
+
+    # tuning the model
+    # regressor = RandomForestRegressor()
+    # grid_search = GridSearchCV(estimator=regressor, param_grid=parameters, cv=3, n_jobs=-1, verbose=2, scoring=make_scorer(r2_score))
+    # grid_search.fit(X_train, y_train)
+    # print("Best parameters: ", grid_search.best_params_)
+    # predictor = grid_search.best_estimator_
+
+    # using the current best hyperparameters
+    predictor = RandomForestRegressor(**parameters)
+    predictor.fit(X_train, y_train)
+
+    # evaluation
+    y_pred = predictor.predict(X_test)
+    np.set_printoptions(precision=2)
+    print(np.concatenate((y_pred.reshape(len(y_pred),1),y_test.reshape(len(y_test),1)),1))
+    print(r2_score(y_test, y_pred))
+
+    # saving the model
+    joblib.dump(predictor, 'pickle_files/model.pkl')
+    joblib.dump(scaler, 'pickle_files/scaler.pkl')
+    joblib.dump(encoder, 'pickle_files/encoder.pkl')
+
+train()
